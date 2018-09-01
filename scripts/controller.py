090fdedb817e0eae3be34362ce7cc0ae41bda64b
Rizwan Asif <rizasif@gmail.com>
2018-09-01 16:28:41 +0200

    controller

--- scripts/controller.py
+++ scripts/controller.py
@@ -11,9 +11,25 @@ import math
 ticks = 360
 b = 0.115
 r = 0.0352
+freq = 10
 
-pub = rospy.Publisher('/kobuki/pwm', PWM, queue_size=10)
 rate = 0
+# is_initialized = False
+
+pwm = PWM()
+
+# desired velocity
+lind = 0
+angd = 0
+
+# error sum
+error_sum1 = 0.0
+error_sum2 = 0.0
+
+# controller
+Kp = 0.2
+Ki = 0.1
+
 
 # def talker():
 # 	pub = rospy.Publisher('/kobuki/pwm', PWM, queue_size=10)
@@ -26,39 +42,57 @@ rate = 0
 # 		pub.publish(pwm)
 # 		rate.sleep()
 
-def callback(data):
+def callback_encoder(data):
+	rospy.loginfo(rospy.get_caller_id() + "Encoder Data recieved: {} {}".format(data.delta_encoder1, data.delta_encoder2))
+	w1 = (float(2)*math.pi*r*data.delta_encoder1*freq)/ticks
+	w2 = (float(2)*math.pi*r*data.delta_encoder2*freq)/ticks
+
+	lina = (w1+w2)/2.0
+	anga = (w2-w1)/(2.0*b)
+
+	elin = lind-lina
+	eang = angd-anga
+
+	evw1 = elin - (b*eang)
+	evw2 = elin + (b*eang)
+
+	error_sum1 += float(evw1)
+	error_sum2 += float(evw2)
+
+	pwm.PWM1 = (Kp*evw1) + (Ki*error_sum1)
+	pwm.PWM2 = (Kp*evw2) + (Ki*error_sum2)
+
+	rospy.loginfo("Publishing PWM: {}, {}".format(pwm.PWM1,pwm.PWM2) )
+	pub.publish(pwm)
+	rate.sleep()
+	
+
+def callback_pwm(data):
 
 	v = data.linear
 	w = data.angular
 
-	rospy.loginfo("Received Command: {} {}".format(v, w))
+	lind = v.x
+	angd = w.x
 
-	encoder = GetEncoderData()
-	pwm = PWM()
+	# rospy.loginfo("Received Command: {} {}".format(v, w))
 
-	vm = (v.x+v.y)/float(2)
-	wm = (w.y-w.x)/(float(2)*b)
-
-	pwm.PWM1 = (float(2)*math.pi*r*encoder.delta_encoder1)/ticks
-	pwm.PWM2 = (float(2)*math.pi*r*encoder.delta_encoder2)/ticks
-
-	rospy.loginfo("Publishing PWM: {}, {}".format(pwm.PWM1,pwm.PWM2) ) 
-	pub.publish(pwm)
-	rate.sleep()
+	# rospy.loginfo("Publishing PWM: {}, {}".format(pwm.PWM1,pwm.PWM2) ) 
+	# pub.publish(pwm)
+	# rate.sleep()
 	
 def listener():
 	# rospy.init_node('pwm_listener', anonymous=True)
-	rospy.Subscriber("/motor_controller/twist", Twist, callback)
+	rospy.Subscriber("/motor_controller/twist", Twist, callback_pwm)
+	rospy.Subscriber("/kobuki/encoders", Encoders, callback_encoder)
 
 	# rospy.spin()
 
-def initialize():
-	pwm = PWM()
-	while not rospy.is_shutdown():
-		pwm.PWM1 = 100
-		pwm.PWM2 = 100
-		pub.publish(pwm)
-		rate.sleep()
+# def keep_moving():
+# 	pub = rospy.Publisher('/kobuki/pwm', PWM, queue_size=10)
+# 	while not rospy.is_shutdown():
+# 		pub.publish(pwm)
+# 		rate.sleep()
 
 	# rospy.spin()
 
@@ -66,10 +100,10 @@ if __name__ == '__main__':
 	try:
 		# talker()
 		rospy.init_node('controller_node', anonymous=True)
-		rate = rospy.Rate(10)
+		rate = rospy.Rate(freq)
 		
-		initialize()
-		# listener()
+		# initialize()
+		listener()
 
 		rospy.spin()
 
