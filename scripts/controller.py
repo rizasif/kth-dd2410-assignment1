8c9c384d14e62d81f332103e92765d73ff998632
Rizwan Asif <rizasif@gmail.com>
2018-09-02 11:40:33 +0200

    controller

--- scripts/controller.py
+++ scripts/controller.py
@@ -68,7 +68,7 @@ def callback_encoder(data):
 	vw1 = (float(2)*math.pi*r*data.delta_encoder1*freq)/ticks
 	vw2 = (float(2)*math.pi*r*data.delta_encoder2*freq)/ticks
 	
-	# rospy.loginfo("w1, w2: {} {}".format(vw1, vw2))
+	rospy.loginfo("w1, w2: {} {}".format(vw1, vw2))
 	
 	# lina = (w1+w2)/2.0
 	# anga = (w2-w1)/(2.0*b)
