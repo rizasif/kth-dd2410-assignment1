dac9321f7b46c56ab5d7e106203221a96c7626d9
Rizwan Asif <rizasif@gmail.com>
2018-09-03 12:10:10 +0200

    controller

--- scripts/wallfollower.py
+++ scripts/wallfollower.py
@@ -36,7 +36,7 @@ def callback_adc(data):
 	twist = Twist()
 	twist.linear.x = speed
 
-	twist.angular.x = last_theta - theta
+	twist.angular.x = theta - last_theta
 	last_theta = theta
 
 	rospy.loginfo("Publishing: {}, {}".format(twist.linear.x,twist.angular.x) )
