68340b2c6a7d37f0ce0b6e902ab63b0bf4254b23
Rizwan Asif <rizasif@gmail.com>
2018-09-04 13:37:56 +0200

    wallfollower

--- scripts/wallfollower.py
+++ scripts/wallfollower.py
@@ -36,15 +36,15 @@ def callback_adc(data):
 	twist = Twist()
 	twist.linear.x = speed
 
-	avg = (d1+d2)/2.0
-	if(avg > 0.3 and theta < 0):
-		rospy.loginfo("Inverting: {}, {}".format(avg,theta))
-		theta *= -1
-	elif(avg < 0.3 and theta > 0):
-		rospy.loginfo("Inverting: {}, {}".format(avg,theta))
-		theta *= -1
+	# avg = (d1+d2)/2.0
+	# if(avg > 0.3 and theta < 0):
+	# 	rospy.loginfo("Inverting: {}, {}".format(avg,theta))
+	# 	theta *= -1
+	# elif(avg < 0.3 and theta > 0):
+	# 	rospy.loginfo("Inverting: {}, {}".format(avg,theta))
+	# 	theta *= -1
 
-	twist.angular.x = theta
+	twist.angular.x = 0.75*theta
 
 	last_theta = theta
 
