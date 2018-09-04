e7666a90efbcf3b58f2a06043bc648957e6c7530
Rizwan Asif <rizasif@gmail.com>
2018-09-04 14:16:15 +0200

    wallfollower

--- scripts/wallfollower.py
+++ scripts/wallfollower.py
@@ -45,6 +45,7 @@ def callback_adc(data):
 	# 		rospy.loginfo("Inverting: {}, {}".format(avg,theta))
 	# 		theta *= -1
 
+	theta = num
 	if(abs(num) <= 0.05 and avg > 0.28 and avg < 0.32):
 		rospy.loginfo("Setting theta null: {} {}".format(abs(num), avg))
 		theta = 0.0
